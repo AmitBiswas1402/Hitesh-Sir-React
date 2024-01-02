d408b68745ae84427042be6807b12e6036ff7590
Amit Biswas <115923314+AmitBiswas1402@users.noreply.github.com>
2024-01-02 12:25:02 +0000

    starting the reactjs and vitejs framework by installing npx and npm

---
+++ customReact/customReact.js
@@ -0,0 +1,10 @@
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://gooogle.com',
+        target: '_blank'
+    },
+    children: 'Click to visit google'
+}
+
+const mainContainer = document.querySelector('#root')
